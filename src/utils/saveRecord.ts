8aba7eafc7c43a06ab939af1b4fd0e8cc038e3ad
Zar Hlyan Aung <zarhlyanaung.ai@gmail.com>
2024-03-10 22:22:22 +0630

    add time to localstorage

--- src/utils/saveRecord.ts
+++ src/utils/saveRecord.ts
@@ -1,4 +1,9 @@
 export const saveRecord = (minutes: number) => {
   //save minutes to local storage with json stringify
-  localStorage.setItem('minutes', JSON.stringify(minutes));
+  if (localStorage.getItem('minutes') === null) {
+    localStorage.setItem('minutes', JSON.stringify(minutes));
+  } else {
+    const minutesSaved = JSON.parse(localStorage.getItem('minutes') || '0');
+    localStorage.setItem('minutes', JSON.stringify(minutesSaved + minutes));
+  }
 };
